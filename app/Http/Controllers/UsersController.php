89b01547f898a967b94ae24b09c11c3e3854a41e
haibingqingxie <jasonli@seasidecrab.com>
2019-01-22 09:54:23 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,4 +18,16 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    // 保存注册信息
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        return;
+    }
 }
