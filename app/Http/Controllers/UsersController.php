4d491879e0150fd0a761d93f0a5206169f4d08db
haibingqingxie <jasonli@seasidecrab.com>
2019-01-22 09:43:50 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     // 注册用户
     public function create()
     {
-        return view('users/create');
+        return view('users.create');
+    }
+
+    // 展示用户
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
