2bffe0fa2dac6ba0fd47a9145a44275c29faa00d
haibingqingxie <jasonli@seasidecrab.com>
2019-01-23 08:37:36 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,8 +54,15 @@ class User extends Authenticatable
     // 动态流原型，当前用户关注的人发布过的所有微博动态
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+
+        return Status::whereIn('user_id', $user_ids)
+                            ->with('user')
+                            ->orderBy('created_at', 'desc');
+        // return $this->statuses()
+        //             ->orderBy('created_at', 'desc');
+
     }
 
     // 用户粉丝关联关系
